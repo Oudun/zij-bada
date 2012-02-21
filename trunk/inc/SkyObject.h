0761a7e407ecf6d365ec301f922dba78ba75be72
ddreval@gmail.com <ddreval@gmail.com@6930c130-1280-01f7-24d8-afae7b0c8ac0>
2012-02-21 03:43:15 +0000

    update

--- trunk/inc/SkyObject.h
+++ trunk/inc/SkyObject.h
@@ -24,16 +24,40 @@ class SkyObject
 public:
 	SkyObject();
 	virtual ~SkyObject();
+
+	// Setting right ascension using whole hour and minute and seconds with decimals as input
 	void setRA(Osp::Base::String& raH, Osp::Base::String&  raM, Osp::Base::String&  raS);
-	void test(void);
+	// Setting declination
+	void setDE(Osp::Base::String& raH, Osp::Base::String&  raM, Osp::Base::String&  raS);
+	// Sets name
+	void setName(Osp::Base::String& name);
+	// sets Draper name
+	void setDraperName(Osp::Base::String& draperName);
+	// Sets true if object in northern hemisphere
+	void setSign(bool isNorthern);
+	// Sets visual magnitude, i.e. brightness of object
+	void setMagnitude(Osp::Base::String& magnitudeStr);
+
+	// Returns right ascension in hours in decimal format
 	float getRAH(void);
+	// Returns declination in degrees in decimal format
+	float getDED(void);
+	// Returns name
+	Osp::Base::String& getName(void);
+	// Returns Draper name
+	Osp::Base::String& getDraperName(void);
+	// Returns true id object in Northern hemisphere
+	bool isNorthern(void);
+	// Returns visual magnitude (brightness). Lesser values have brighter stars
+	float getMagnitude(void);
 
 private:
 	Osp::Base::String name;
 	Osp::Base::String draperName;
 	float RAH;
-	float RAM;
-	float RAS;
+	float DED;
+	float magnitude;
+	bool sign;
 //    String name = str.substring(5, 14);
 //    String draperName = str.substring(25, 31);
 //    String rahStr = str.substring(75,77);
