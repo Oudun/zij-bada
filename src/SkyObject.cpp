20674c259969613299645426c8bae3476186dde7
ddreval <ddreval@gmail.com>
2012-02-21 03:43:02 +0000

    update

--- src/SkyObject.cpp
+++ src/SkyObject.cpp
@@ -17,11 +17,6 @@ SkyObject::~SkyObject() {
 }
 
 void
-SkyObject::test(void) {
-	AppLog("TEST");
-}
-
-void
 SkyObject::setRA(Osp::Base::String& raH, Osp::Base::String&  raM, Osp::Base::String&  raS)
 {
 	float f1, f2, f3;
@@ -29,9 +24,63 @@ SkyObject::setRA(Osp::Base::String& raH, Osp::Base::String&  raM, Osp::Base::Str
 	Osp::Base::Float::Parse(raM, f2);
 	Osp::Base::Float::Parse(raS, f3);
 	RAH = f1 + (f2/60) + (f3/3600);
+	AppLog("Right Accentation is %f", RAH);
 }
 
 float
 SkyObject::getRAH(void) {
 	return RAH;
+}
+
+void
+SkyObject::setDE(Osp::Base::String& deD, Osp::Base::String&  deM, Osp::Base::String&  deS)
+{
+	float f1, f2, f3;
+	Osp::Base::Float::Parse(deD, f1);
+	Osp::Base::Float::Parse(deM, f2);
+	Osp::Base::Float::Parse(deS, f3);
+	DED = f1 + (f2/60) + (f3/3600);
+	AppLog("Declination is %f", DED);
+}
+
+void
+SkyObject::setMagnitude(Osp::Base::String& magnitudeStr) {
+	float f;
+	Osp::Base::Float::Parse(magnitudeStr, f);
+	magnitude = f;
+}
+
+void
+SkyObject::setSign(bool isNorthern) {
+	sign = isNorthern;
+}
+
+void
+SkyObject::setName(Osp::Base::String& aName) {
+	name = aName;
+}
+
+void
+SkyObject::setDraperName(Osp::Base::String& aDraperName) {
+	draperName = aDraperName;
+}
+
+Osp::Base::String&
+SkyObject::getName(void) {
+	return name;
+}
+
+Osp::Base::String&
+SkyObject::getDraperName(void) {
+	return draperName;
+}
+
+bool
+SkyObject::isNorthern(void) {
+	return sign;
+}
+
+float
+SkyObject::getMagnitude(void) {
+	return magnitude;
 }
