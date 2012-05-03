9104cea0d26f69adc10bed2bc93fa69ccee4ec4a
ddreval@gmail.com <ddreval@gmail.com@6930c130-1280-01f7-24d8-afae7b0c8ac0>
2012-05-03 14:23:18 +0000

    sky drawing via rotated bitmap now

--- trunk/src/SkyForm.cpp
+++ trunk/src/SkyForm.cpp
@@ -57,25 +57,10 @@ SkyForm::OnInitializing(void)
 		__pButtonZoomOut->AddActionEventListener(*this);
 	}
 
-//	Button *pButton_gps = static_cast<Button *>(GetControl("IDC_BUTTON_GPS"));
-//	if (pButton_gps)
-//	{
-//		pButton_gps->SetActionId(ID_BUTTON_GPS);
-//		pButton_gps->AddActionEventListener(*this);
-//	}
-
 	__pZoomLabel = static_cast<Label *>(GetControl("IDC_LABEL_ZOOM"));
-
-//	Button *pButton1 = static_cast<Button *>(GetControl("IDC_BUTTON_STARS"));
-//	if (pButton1)
-//	{
-//		pButton1->SetActionId(103);
-//		pButton1->AddActionEventListener(*this);
-//	}
 
 	Control* control = GetControl(L"IDF_FORM1");
 	sky = new Sky(control->GetCanvasN());
-	sky->paintBorders();
 	locProvider.Construct(LOC_METHOD_HYBRID);
 	locProvider.RequestLocationUpdates(*this, 5, true);
 
@@ -134,45 +119,29 @@ void
 SkyForm::OnLocationUpdated(Osp::Locations::Location& location) {
 	AppLog("Location Updated\n");
 	const QualifiedCoordinates* coordinates = location.GetQualifiedCoordinates();
-	AppLog("Coordinates taken\n");
 	String str;
 	if (coordinates != 0) {
-		double latd = coordinates->GetLatitude();
-		AppLog("Latitude (double) taken\n");
-		AppLog("Latitude (float) taken\n");
-		Log("Latitude = ",(float)(coordinates->GetLatitude()));
-		Log("Longitude = ",(float)(coordinates->GetLongitude()));
+		AppLog("Coordinates taken\n");
 		sky->setLatitude(coordinates->GetLatitude());
 		sky->setLongitude(coordinates->GetLongitude());
-		AppLog("Local Siderial Hours is ", sky->getSiderialHours());
-		//		Log("SLT = ", GetLocalSiderialTime((float)(coordinates->GetLongitude())));
 		locProvider.CancelLocationUpdates();
-//		sky->setLatitude(55.75578F);
-//		sky->setLongitude(37.8632F);
-		AppLog("Local Siderial Hours is %f", sky->getSiderialHours());
-		Log("Local Siderial Hours is ", sky->getSiderialHours());
 		sky->draw();
 	} else {
-		LogSameLine("#");
+		AppLog("#");
 	}
 }
 
 void
 SkyForm::OnProviderStateChanged(Osp::Locations::LocProviderState newState) {
 	AppLog("Location Provider state changed\n");
-	Log("Location Provider state changed\n");
 	if (newState == LOC_PROVIDER_AVAILABLE) {
 		AppLog("The location provider is available");
-		Log("The location provider is available");
 	} else if (newState == LOC_PROVIDER_OUT_OF_SERVICE) {
 		AppLog("The location provider is out of service");
-		Log("The location provider is out of service");
 	} else if (newState == LOC_PROVIDER_TEMPORARILY_UNAVAILABLE) {
 		AppLog("The location provider is temporarily unavailable");
-		Log("The location provider is temporarily unavailable");
 	} else {
 		AppLog("State unknown");
-		Log("State unknown");
 	}
 }
 
@@ -181,8 +150,6 @@ SkyForm::GetLocalSiderialTime(float longitude) {
 	TimeZone timeZone(60, L"Europe/London");
 	DateTime* currTime = new DateTime();
 	SystemTime::GetCurrentTime(*currTime);
-	LogSameLine("currTime=");
-	Log(currTime->ToString());
 	calendar = Calendar::CreateInstanceN(timeZone, CALENDAR_GREGORIAN);
 	calendar->SetTime(*currTime);
 	float hrs = (calendar->GetTimeField(TIME_FIELD_HOUR_OF_DAY))-1;
@@ -194,47 +161,9 @@ SkyForm::GetLocalSiderialTime(float longitude) {
 	double slt = 100.46 + 0.985647 * daysSinceJ2000 + longitude + 15*(hrs + minHrs);
 	int sltInt = (int)(slt/360);
 	float sltHrs = (slt-(360*sltInt))/15;
-	Log("lstHours=",sltHrs);
 	return sltHrs;
 }
 
-void
-SkyForm::Log(const Osp::Base::String& aText, const float value) {
-//	String str;
-//	String text;
-//	text = aText;
-//	str = __pLabel->GetText();
-//	text.Append(value);
-//	str.Append(text);
-//	str.Append('\n');
-//	__pLabel->SetText(str);
-//	__pLabel->RequestRedraw();
-}
-
-void
-SkyForm::Log(const Osp::Base::String& text) {
-//	String str;
-//	str = __pLabel->GetText();
-//	str.Append(text);
-//	str.Append('\n');
-//	__pLabel->SetText(str);
-//	__pLabel->RequestRedraw();
-}
-
-void
-SkyForm::ClearLog(const Osp::Base::String& text) {
-//	__pLabel->SetText(text);
-//	__pLabel->RequestRedraw();
-}
-
-void
-SkyForm::LogSameLine(const Osp::Base::String& text) {
-//	String str;
-//	str = __pLabel->GetText();
-//	str.Append(text);
-//	__pLabel->SetText(str);
-//	__pLabel->RequestRedraw();
-}
 
 
 
